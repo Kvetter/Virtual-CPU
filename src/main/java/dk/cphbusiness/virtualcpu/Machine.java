147b74c50b551057229a2880a417e69394518184
Kasper Vetter <kasper.vetter@mail.dk>
2017-05-25 17:15:24 +0200

    Finished the CPU

--- src/main/java/dk/cphbusiness/virtualcpu/Machine.java
+++ src/main/java/dk/cphbusiness/virtualcpu/Machine.java
@@ -56,72 +56,81 @@ public class Machine {
             if (cpu.getA() != 0) {
                 cpu.setFlag(true);
             }
+            cpu.incIp();
         } else if (instr == 0b0000_1000) {
-           // 0000 1000	EQ	F ← A = B; IP++
+            // 0000 1000	EQ	F ← A = B; IP++
             if (cpu.getA() == cpu.getB()) {
                 cpu.setFlag(true);
             }
+            cpu.incIp();
         } else if (instr == 0b0000_1001) {
             // 0000 1001	LT	F ← A < B; IP++
             if (cpu.getA() < cpu.getB()) {
                 cpu.setFlag(true);
             }
+            cpu.incIp();
         } else if (instr == 0b0000_1010) {
             // 0000 1010	GT	F ← A > B; IP++
             if (cpu.getA() > cpu.getB()) {
                 cpu.setFlag(true);
             }
+            cpu.incIp();
         } else if (instr == 0b0000_1011) {
             // 0000 1011	NEQ	F ← A ≠ B; IP++
             if (cpu.getA() != cpu.getB()) {
                 cpu.setFlag(true);
             }
+            cpu.incIp();
         } else if (instr == 0b0000_1100) {
             // 0000 1100	ALWAYS	F ← true; IP++
             cpu.setFlag(true);
             cpu.incIp();
         } else if (instr == 0b0000_1111) {
             // 0000 1111	HALT	Halts execution
-            return; // ???????
-        }  else if ((instr & 0b1111_1110) == 0b0001_0000) {
+            System.out.println("Halt?");
+            System.exit(0);
+        } else if ((instr & 0b1111_1110) == 0b0001_0000) {
             //  0001 000r	PUSH r	[--SP] ← r; IP++
+            cpu.decSp();
             int r = (instr & 0b0000_0001); //////????????????????????
             if (r == cpu.A) {
-                memory.set(cpu.getSp() - 1, cpu.getA());
+                memory.set(cpu.getSp(), cpu.getA());
             } else {
-               memory.set(cpu.getSp() - 1, cpu.getB());
+                memory.set(cpu.getSp(), cpu.getB());
             }
             cpu.incIp();
-            cpu.decSp();
-        }  else if ((instr & 0b1111_1110) == 0b0001_0010) {
+        } else if ((instr & 0b1111_1110) == 0b0001_0010) {
             // 0001 001r	POP r	r ← [SP++]; IP++
-            int r = (instr & 0b0000_0010); // ?????????
-             if (r == cpu.A) {
-                 cpu.setA(memory.get(cpu.getSp() + 1));
-             } else {
-                 cpu.setB(memory.get(cpu.getSp() + 1));
-             }
-        }  else if (instr == 0b0001_0100) {
+            int r = (instr & 0b0000_0001);
+            if (r == cpu.A) {
+                cpu.setA(memory.get(cpu.getSp()));
+            } else {
+                cpu.setB(memory.get(cpu.getSp()));
+            }
+            cpu.setSp(cpu.getSp() + 1);
+            cpu.incIp();
+        } else if (instr == 0b0001_0100) {
             // 0001 0100	MOV A B	B ← A; IP++
             cpu.setB(cpu.getA());
             cpu.incIp();
-        }  else if (instr == 0b0001_0101) {
+        } else if (instr == 0b0001_0101) {
             // 0001 0101	MOV B A	A ← B; IP++
             cpu.setA(cpu.getB());
             cpu.incIp();
-        }  else if (instr == 0b0001_0110) {
+        } else if (instr == 0b0001_0110) {
             // 0001 0110	INC	A++; IP++
             cpu.setA(cpu.getA() + 1);
             cpu.incIp();
-        }  else if (instr == 0b0001_0111) {
+        } else if (instr == 0b0001_0111) {
             // 0001 0111	DEC	A--; IP++
             cpu.setA(cpu.getA() - 1);
             cpu.incIp();
-        }  else if ((instr & 0b1111_1000) == 0b0001_1000) {
+        } else if ((instr & 0b1111_1000) == 0b0001_1000) {
             // 0001 1ooo	RTN +o	IP ← [SP++]; SP += o; IP++
-            
-        } 
-        else if ((instr & 0b1111_0000) == 0b0010_0000) {
+            int o = instr & 0b0000_0111;
+            cpu.setIp(memory.get(cpu.getSp() + 1));
+            cpu.setSp(cpu.getSp() + 0 + 1);
+        } else if ((instr & 0b1111_0000) == 0b0010_0000) {
             // 0010 r ooo	MOV r o	   [SP + o] ← r; IP++
 
             // 0010 1 011 MOV B (=1) +3  [SP +3] // Move register B to memory position of SP with offset 3
@@ -150,6 +159,50 @@ public class Machine {
                 memory.set(cpu.getSp() + o, cpu.getB());
             }
             cpu.incIp();
+        } else if((instr & 0b1111_0000) == 0b0011_0000){
+            int r = (instr & 0b0000_0001);
+            int o = (instr & 0b0000_1110) >> 1;
+            
+            if (r == cpu.A) {
+                cpu.setA(memory.get(cpu.getSp() + o));
+            } else {
+                cpu.setB(memory.get(cpu.getSp() + o));
+            }
+            cpu.incIp();
+        } else if ((instr & 0b1100_0000) == 0b0100_0000) {
+            int v = ((instr & 0b0011_1110) >> 1);
+            if (((instr & 0b0010_0000)) == 0b0010_0000) {
+                v = -((instr & 0b0001_1110) >> 1);
+            }
+
+            System.out.println("v = " + v);
+            int r = (instr & 0b0000_0001);
+            if (r == cpu.A) {
+                cpu.setA(v);
+            } else {
+                cpu.setB(v);
+            }
+            cpu.incIp();
+        } else if ((instr & 0b1100_0000) == 0b1000_0000) {
+            System.out.println("JUMP #a");
+            if (cpu.isFlag()) {
+                int a = instr & 0b0011_1111;
+                cpu.setIp(a);
+            } else {
+                cpu.incIp();
+            }
+        } //CALL #a: if F then [--SP] ← IP; IP ← a else IP++
+        //11aa aaaa
+        else if ((instr & 0b1100_0000) == 0b1100_0000) {
+            System.out.println("CALL #a");
+            if (cpu.isFlag()) {
+                //Maybe decrease the Stack pointer first?
+                cpu.decSp();
+                memory.set(cpu.getSp(), cpu.getIp());
+                cpu.setIp((instr & 0b0011_1111));
+            } else {
+                cpu.incIp();
+            }
         }
     }
 
--- src/main/java/dk/cphbusiness/virtualcpu/Main.java
+++ src/main/java/dk/cphbusiness/virtualcpu/Main.java
@@ -1,18 +1,38 @@
 package dk.cphbusiness.virtualcpu;
 
 public class Main {
-  
-  public static void main(String[] args) {
-    System.out.println("Welcome to the awesome CPU program");
-    Program program = new Program("00101001", "00001111", "10101010");
-    Machine machine = new Machine();
-    machine.load(program);
-    machine.print(System.out);
-    machine.tick();
-    machine.print(System.out);
+    
+    // Got the program from Mikkel Ziemer
 
-    for (int line : program) System.out.println(">>> "+line);
-    
+    public static void main(String[] args) {
+        System.out.println("Welcome to the awesome CPU program");
+        //Program program = new Program("00101001", "00001111", "10101010", "MOV B +3");
+        //(5 + 11)*-3
+        Program add = new Program("01001010", "00010000", "01010110", "00010000", 
+                "01100110", "00010000", "00001100", "11001010",
+                "00010010", "00001111", "00110111", "00110100", "00000001",
+                "00110011", "00000010", "00100011", "00011010");
+        
+        Program fact = new Program("01001010", "00010000", "00001100", "11000110",
+                "00010010", "00001111", "00110010", "00000111", "10001100", 
+                "01000010", "00100001", "00011000", "00010000", "00010111", 
+                "00010000", "00001100", "11000110", "00010011", "00010010", 
+                "00000010", "00100001", "00011000");
+        
+        //Tail recursive factorial of 5
+        Program tailFact = new Program("01000010", "00010000", "01001010", "00010000", 
+                "00001100", "11001000", "00010010", "00001111", 
+                "00110010", "00000111", "10001100", "00011001", 
+                "00110101", "00000010", "00100010", "00110010", "00010111", "00100001", "00001100", "10001000");
+        Machine machine = new Machine();
+        
+        
+        machine.load(tailFact);
+        machine.print(System.out);
+        while(true){
+            machine.tick();
+            machine.print(System.out);
+        }
     }
-    
-  }
+
+}
